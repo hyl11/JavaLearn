66c01d873801dcdfe4502db23c589d327160aedb
ocean <hyl11@users.noreply.github.com>
2018-09-20 19:27:31 +0800

    增加了查询功能

---
+++ learn/src/com/stuman/ShowView.java
@@ -0,0 +1,96 @@
+package com.stuman;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import java.util.Vector;
+
+/*
+ * 1，界面设计，整体分成三个部分，上中下，上下为两个JPanel，中间是一个JTable
+ *    （1）上面的JPanel放置组件，JTextField，文本输入框，
+ *                           JLabel,     提示信息
+ *                           JButton，       查询按钮
+ *    （2）下面的JPanel放置的组件，三个JButton，分别代表删除，增加，修改
+ *    （3）中间的JTable用model的构造函数初始化，这样可以刷新Table上的数据,
+ *        并且加入到JScrollPanel中，可以上下滚动
+ *        
+ * */
+
+/*
+ * 加入监听机制，因为操作需要刷新JTable，所以在JFrame上增加监听功能
+ * 监听的操作包括四个JButton的点击
+ * 
+ * */
+
+/*
+ * 新建StuModel类，用来读取相关的数据库数据并且构造成为model类，可以直接初始化Table
+ * */
+public class ShowView extends JFrame implements ActionListener {
+
+	/*定义相关的界面组件*/
+	JPanel NorthPanel=null,SouthPanel=null;
+	JTable Table=null;
+	JTextField InputField=null;
+	JLabel Meg = null;
+	JButton Query=null,Delete=null,Add=null,Change=null;
+	JScrollPane Scroll = null;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("b");
+		ShowView sv = new ShowView();
+	}
+	
+	public ShowView() {
+		System.out.println("a");
+		/*初始化NorthPanel*/
+		NorthPanel = new JPanel();
+		InputField = new JTextField(10);
+		Query = new JButton("query");
+		Query.addActionListener(this);
+		Meg = new JLabel("please input name");
+		NorthPanel.add(Meg);
+		NorthPanel.add(InputField);
+		NorthPanel.add(Query);
+		
+		/*初始化SouthPanel*/
+		SouthPanel = new JPanel();
+		Delete = new JButton("delete");
+		Delete.addActionListener(this);
+		Add = new JButton("add");
+		Add.addActionListener(this);
+		Change = new JButton("change");
+		Change.addActionListener(this);
+		SouthPanel.add(Add);
+		SouthPanel.add(Change);
+		SouthPanel.add(Delete);
+		
+		/*初始化中间的Table*/
+		StuModel Stum = new StuModel("select * from Student");
+		Table=new JTable(Stum);
+		Scroll=new JScrollPane(Table);
+		this.add(NorthPanel,BorderLayout.NORTH);
+		this.add(SouthPanel,BorderLayout.SOUTH);
+		this.add(Scroll);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(400,300);
+		this.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource().equals(Query)) {
+			String name = InputField.getText();
+			StuModel Stum = new StuModel("select * from Student where StuName='" + name + "'");
+			Table = new JTable(Stum);
+			this.add(Table);
+			this.setVisible(true);
+		}
+	}
+
+}
---
+++ learn/src/com/stuman/StuModel.java
@@ -0,0 +1,78 @@
+package com.stuman;
+
+import java.util.Vector;
+
+import javax.swing.table.AbstractTableModel;
+import java.sql.*;
+public class StuModel extends AbstractTableModel {
+
+	@Override
+	public String getColumnName(int column) {
+		// TODO Auto-generated method stub
+		return (String)colName.get(column);
+	}
+
+	/*定义查询数据想干结构*/
+	String url="jdbc:sqlserver://127.0.0.1:1433;databaseName=Testbase2";
+	Vector rowData,colName;
+	ResultSet rs = null;
+	
+	public StuModel(String sql) {
+		colName=new Vector<>();
+		colName.add("StuID");
+		colName.add("StuName");
+		colName.add("StuAge");
+		colName.add("StuDept");
+		
+		rowData=new Vector();
+		PreparedStatement ps = null;
+		Connection ct = null;
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			ct=DriverManager.getConnection(url,"sa","7");
+			ps=ct.prepareStatement(sql);
+			rs=ps.executeQuery();
+			while(rs.next()) {
+				Vector line=new Vector<>();
+				line.add(rs.getString(1));
+				line.add(rs.getString(2));
+				line.add(rs.getString(3));
+				line.add(rs.getString(4));
+				rowData.add(line);
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}finally {
+			try {
+				ct.close();
+				ps.close();
+				rs.close();
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+			
+		}
+	}
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return rowData.size();
+	}
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return colName.size();
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		return ((Vector)rowData.get(rowIndex)).get(columnIndex);
+	}
+
+}
