665fec6ff00dabbfad009a16c6e08af118bdb5fa
ocean <hyl11@users.noreply.github.com>
2018-09-21 20:56:27 +0800

    设计model2模型，搭建项目框架

---
+++ StudentManger/src/com/datautility/DataUtility.java
@@ -0,0 +1,14 @@
+package com.datautility;
+
+public class DataUtility {
+
+	public DataUtility() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ StudentManger/src/com/model/StudentModel.java
@@ -0,0 +1,9 @@
+package com.model;
+
+public class StudentModel {
+
+	public StudentModel() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
